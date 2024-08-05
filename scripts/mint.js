2b24c92c771cf08ae1a13021ec129caf7484e7e3
web3kachi <omeradesignsx@gmail.com>
2024-08-05 16:00:35 +0000

    Initial commit

---
+++ scripts/mint.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x2c2b32d2FBC6fd1601D43a93C7cFF5D98aef3850";
+  const [signer] = await hre.ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = contractFactory.attach(contractAddress);
+
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+
+  await mint100TokensTx.wait();
+
+  console.log("Transaction Receipt: ", mint100TokensTx.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
